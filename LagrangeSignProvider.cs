f67738caab856a78710e5da1da1921d8a7cd5059
DreamPrism <thelastprism@foxmail.com>
2023-11-17 22:55:59 +0800

    init commit: implemented login logic

---
+++ LagrangeSignProvider.cs
@@ -0,0 +1,72 @@
+﻿using Lagrange.Core.Utility.Sign;
+using System.Globalization;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Text;
+using System.Web;
+
+internal class LagrangeSignProvider : SignProvider
+{
+    private const string Url = "https://sign.libfekit.so/api/sign";
+
+    private readonly HttpClient _client = new();
+
+    public override byte[] Sign(string cmd, uint seq, byte[] body, out byte[] ver, out string token)
+    {
+        ver = null;
+        token = null;
+        if (!WhiteListCommand.Contains(cmd)) return null;
+        if (string.IsNullOrEmpty(Url)) return new byte[20]; // Dummy signature
+
+        try
+        {
+            var payload = new Dictionary<string, string>
+            {
+                { "cmd", cmd },
+                { "seq", seq.ToString() },
+                { "src", Hex(body) },
+            };
+            var response = _client.GetAsync(BuildUrl(Url, payload)).GetAwaiter().GetResult();
+            string raw = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            var json = JsonSerializer.Deserialize<JsonObject>(raw);
+
+            return UnHex(json?["value"]?["sign"]?.ToString() ?? "");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{nameof(LagrangeSignProvider)}] Failed to get signature, using dummy signature");
+            return new byte[20]; // Dummy signature
+        }
+    }
+
+    private static Uri BuildUrl(string url, Dictionary<string, string> payload)
+    {
+        var uriBuilder = new UriBuilder(url);
+
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        foreach (var (key, value) in payload) query[key] = value;
+        uriBuilder.Query = query.ToString();
+
+        return uriBuilder.Uri;
+    }
+
+    private static string Hex(byte[] bytes, bool lower = false, bool space = false)
+    {
+        var sb = new StringBuilder();
+        foreach (byte b in bytes)
+        {
+            sb.Append(b.ToString(lower ? "x2" : "X2"));
+            if (space) sb.Append(' ');
+        }
+        return sb.ToString();
+    }
+
+    private static byte[] UnHex(string hex)
+    {
+        if (hex.Length % 2 != 0) throw new ArgumentException("Invalid hex string");
+
+        byte[] bytes = new byte[hex.Length / 2];
+        for (int i = 0; i < hex.Length; i += 2) bytes[i / 2] = byte.Parse(hex.Substring(i, 2), NumberStyles.HexNumber);
+        return bytes;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using Lagrange.Core;
+using Lagrange.Core.Common;
+using Lagrange.Core.Common.Interface;
+using Lagrange.Core.Common.Interface.Api;
+using Lagrange.Core.Internal.Event.EventArg;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Lagrange.HikawaHina;
+class Program
+{
+    // private const string CONFIG_PATH = "appsettings.json";
+    private static readonly string BotFilesPath = "bot";
+    private static string DeviceInfoPath => Path.Combine(BotFilesPath, "device.json");
+    private static string KeyStorePath => Path.Combine(BotFilesPath, "keystore.json");
+
+    private static BotDeviceInfo _deviceInfo;
+    private static BotKeystore _keyStore;
+    private static BotContext _bot;
+
+    private static readonly LogLevel _defaultLogLevel = LogLevel.Information;
+    private static void InitBotInfo()
+    {
+        if (!Directory.Exists(BotFilesPath))
+            Directory.CreateDirectory(BotFilesPath);
+
+        // Load device info
+        if (File.Exists(DeviceInfoPath))
+        {
+            var info = JsonSerializer.Deserialize<BotDeviceInfo>(File.ReadAllText(DeviceInfoPath));
+            if (info == null)
+            {
+                info = BotDeviceInfo.GenerateInfo();
+                File.WriteAllText(DeviceInfoPath, JsonSerializer.Serialize(info));
+            }
+            _deviceInfo = info;
+        }
+
+        // Load key store
+        if (File.Exists(KeyStorePath))
+        {
+            var keyStore = JsonSerializer.Deserialize<BotKeystore>(File.ReadAllText(KeyStorePath),
+                new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.Preserve });
+            if (keyStore == null)
+            {
+                keyStore = new();
+                File.WriteAllText(KeyStorePath, JsonSerializer.Serialize(keyStore));
+            }
+            _keyStore = keyStore;
+        }
+    }
+    private static void InitBot()
+    {
+        _bot = BotFactory.Create(new()
+        {
+            UseIPv6Network = false,
+            GetOptimumServer = true,
+            AutoReconnect = true,
+            Protocol = Protocols.Linux,
+            CustomSignProvider = new LagrangeSignProvider(),
+        }, _deviceInfo, _keyStore);
+
+        _bot.Invoker.OnBotLogEvent += (context, @event) =>
+        {
+            if (@event.Level >= _defaultLogLevel)
+            {
+                ChangeColorByTitle(@event.Level);
+                Console.WriteLine(@event.ToString());
+            }
+        };
+
+        _bot.Invoker.OnBotOnlineEvent += (context, @event) =>
+        {
+            Console.WriteLine(@event.ToString());
+            SaveKeystore(_bot.UpdateKeystore());
+        };
+    }
+    private static void SaveKeystore(BotKeystore keystore) =>
+        File.WriteAllText(KeyStorePath, JsonSerializer.Serialize(keystore));
+    static async Task Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        InitBotInfo();
+        InitBot();
+
+        await _bot.LoginByPassword();
+    }
+    private static void ChangeColorByTitle(LogLevel level) => Console.ForegroundColor = level switch
+    {
+        LogLevel.Debug => ConsoleColor.White,
+        LogLevel.Verbose => ConsoleColor.DarkGray,
+        LogLevel.Information => ConsoleColor.Blue,
+        LogLevel.Warning => ConsoleColor.Yellow,
+        LogLevel.Fatal => ConsoleColor.Red,
+        _ => Console.ForegroundColor
+    };
+}
